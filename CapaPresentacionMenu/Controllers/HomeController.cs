bbd3e0f401fe7f962a72dd63e003e02f5c173984
DeyvisBenito <deyvisbenito@gmail.com>
2024-02-21 22:52:40 -0600

    Agregacion de los modulos edit y cliente

--- CapaPresentacionMenu/Controllers/HomeController.cs
+++ CapaPresentacionMenu/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MenuGratis.Controllers
+namespace CapaPresentacionMenu.Controllers
 {
     public class HomeController : Controller
     {
