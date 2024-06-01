c01dc4f6d5d0bae07d7b478b2d1d1b1f6169d95b
DeyvisBenito <deyvisbenito@gmail.com>
2024-05-31 21:50:32 -0600

    Vuelta

--- MenuGratis/Global.asax.cs
+++ MenuGratis/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,37 +16,6 @@ namespace MenuGratis
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            SetConnectionString();
-        }
-
-        private void SetConnectionString()
-        {
-            var connectionString = Environment.GetEnvironmentVariable("CLEARDB_DATABASE_URL");
-
-            if (!string.IsNullOrEmpty(connectionString))
-            {
-                var uri = new Uri(connectionString);
-                var userInfo = uri.UserInfo.Split(':');
-
-                var builder = new MySqlConnectionStringBuilder
-                {
-                    Server = uri.Host,
-                    Port = (uint)uri.Port,
-                    Database = uri.AbsolutePath.Trim('/'),
-                    UserID = userInfo[0],
-                    Password = userInfo[1],
-                    SslMode = MySqlSslMode.Required,
-                };
-
-                var settings = ConfigurationManager.ConnectionStrings["conex"];
-                var fi = typeof(ConfigurationElement).GetField("_bReadOnly", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
-                fi.SetValue(settings, false);
-
-                settings.ConnectionString = builder.ConnectionString;
-                fi.SetValue(settings, true);
-            }
         }
     }
 }
-
